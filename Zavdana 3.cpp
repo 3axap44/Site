b3b26fa7c305c31e077a6e9c46946ac0f31c5e61
3axap44 <skadi227@gmail.com>
2019-10-24 09:48:04 +0300

    Komentar

---
+++ Zavdana 3.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+int main()
+{
+	using namespace std;
+	int x = 14;
+	float q, pi = 3.14;
+	q = exp(-exp(1) * log(2 * x)) + pi / 2 - atan(sqrt(exp(1)));
+	cout << q;
+	_getch();
+	return 0;
+}
